0c5d13ff3363a30d77a3b9bc654a8be1773d582e
Gabo2447 <gabrieldeolival2@gmail.com>
2025-11-01 16:23:37 -0400

    feat(main): :sparkles: We add validations to the inputs (email) and create a backend

---
+++ src/actions/index.ts
@@ -0,0 +1,32 @@
+import { saveNewsletterEmail } from "@/newsletter/services/subscribe";
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+export const server = {
+  newsletter: defineAction({
+    input: z.object({
+      email: z.string().email("¡Lo siento! El email no es valido"),
+    }),
+    async handler({ email }) {
+      const { success, duplicated, error } = await saveNewsletterEmail(email);
+
+      if (!success) {
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: error ?? "Error al guardar el email en la newsletter",
+        });
+      }
+
+      if (duplicated) {
+        return {
+          success: true,
+          message: "¡Este usuario ya estaba en la newsletter!",
+        };
+      }
+      return {
+        success: true,
+        message: "¡Te has suscrito a la newsletter!",
+      };
+    },
+  }),
+};
